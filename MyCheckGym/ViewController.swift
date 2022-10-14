25aea7459562b773c6d646fda9c4d1ac050aa1d0
user <user@mb030979.local>
2022-10-14 06:14:47 -0300

    Inicia o projeto

---
+++ MyCheckGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCheckGym
+//
+//  Created by user on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
